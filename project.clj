018633ea40a012a2d6c2b3651f3f3693f6ff035a
osbornecharles <cosborne0731@gmail.com>
2017-03-23 12:40:16 -0400

    javascript

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.494"]
+          ;       [org.clojure/clojurescript "1.9.494"]
                  [cheshire "5.3.1"]
                  [compojure "1.5.1"]
                  [com.novemberain/monger "3.1.0"]
                  [clj-http "2.3.0"]
-                 [cljs-http "0.1.42"]
+            ;     [cljs-http "0.1.42"]
                  [cheshire "5.7.0"]
-                 [com.cognitect/transit-cljs "0.8.239"]
+    ;             [com.cognitect/transit-cljs "0.8.239"]
                  [org.clojure/data.json "0.2.6"]
                  [hiccup "1.0.4"]
                  [ring/ring-json "0.3.1"]
@@ -18,13 +18,13 @@
                  [ring/ring-servlet "1.2.0-RC1"]
                  [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]
-           [lein-cljsbuild "1.1.1"]]
-  :hooks [leiningen.cljsbuild]
-  :cljsbuild {
-              :builds [{:source-paths ["src"]
-                        :compiler {:output-to "resources/public/js/main.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]}
+  ;         [lein-cljsbuild "1.1.1"]]
+  ; :hooks [leiningen.cljsbuild]
+  ; :cljsbuild {
+  ;             :builds [{:source-paths ["src"]
+  ;                       :compiler {:output-to "resources/public/js/main.js"
+  ;                                  :optimizations :whitespace
+  ;                                  :pretty-print true}}]}
   :ring {:handler archie.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
